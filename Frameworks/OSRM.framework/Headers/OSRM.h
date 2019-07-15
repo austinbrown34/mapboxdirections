f3bd80bac0cf3fff8c0e489e5b6e480f646dcbb3
Austin <austinbrown34@gmail.com>
2019-07-15 10:06:18 -0400

    wip

---
+++ Frameworks/OSRM.framework/Headers/OSRM.h
@@ -0,0 +1,22 @@
+//
+//  OSRM.h
+//  OSRM
+//
+//  Created by Romain Quidet on 01/04/2017.
+//  Copyright © 2017 XDAppfactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OSRM.
+FOUNDATION_EXPORT double OSRMVersionNumber;
+
+
+//! Project version string for OSRM.
+FOUNDATION_EXPORT const unsigned char OSRMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSRM/PublicHeader.h>
+#import <OSRM/ResponseKeys.h>
+#import <OSRM/OSRMService.h>
+#import <OSRM/RouteService.h>
+#import <OSRM/NearestService.h>
